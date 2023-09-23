78c1773018310734d5c7ff72c9f39d366bf0fc5a
Vaibhav <dev.vaibhav99@gmail.com>
2023-09-23 16:26:44 +0530

    added db config file and tested the same

---
+++ backend/config/db-config.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: "srv944.hstgr.io",
+  user: "u904598717_dev_ds",
+  password: "Dev_ds@123",
+  database: "u904598717_dev_ds",
+  //   waitForConnections: true,
+  //   connectionLimit: 10, // Adjust this based on your needs
+  //   queueLimit: 0, // 0 means unlimited queue
+});
+
+// Export the pool to use it in your routes/controllers
+module.exports = pool.promise();
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../config/db-config");
+
+router.get("/", async (req, res) => {
+  try {
+    const [rows, fields] = await db.query("SELECT * FROM test");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
