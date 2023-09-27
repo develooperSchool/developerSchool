9440e8b856039ce35bfdc45bcc3502c2434eb8a9
Vaibhav <dev.vaibhav99@gmail.com>
2023-09-27 19:16:53 +0530

    added input validation for payment post api

--- backend/routes/revenue.routes.js
+++ backend/routes/revenue.routes.js
@@ -1,7 +1,11 @@
 var express = require("express");
 const controller = require("../controllers/revenue.controller");
+const util = require("../utils/revenue.utils");
 
 var router = express.Router();
+let status = 0;
+let messsage = "";
+let body = {};
 
 router.get("/", (req, res) => {
   controller.getAllRevenueCategories(req, res);
@@ -17,6 +21,58 @@ router.delete("/delete/:id", (req, res) => {
 
 router.put("/update/:id", (req, res) => {
   controller.udpateRevenueCategoryById(req, res);
+});
+
+router.use("/payment", (req, res, next) => {
+  if (
+    util.isNullOrUndefined(req.body.userId) ||
+    util.isInvalidId(req.body.userId)
+  ) {
+    status = 400;
+    messsage = "Invalid User Id";
+  }
+
+  if (util.isInvalidId(req.body.revenueCategoryId)) {
+    status = 400;
+    messsage = "Invalid Revenue Category Id";
+  }
+
+  if (
+    req.body.revenueCategoryId === 1 ||
+    req.body.revenueCategoryId === 2 ||
+    req.body.revenueCategoryId === 3 ||
+    req.body.revenueCategoryId === 4
+  ) {
+    if (
+      util.isNullOrUndefined(req.body.studentId) ||
+      util.isInvalidId(req.body.studentId)
+    ) {
+      status = 400;
+      messsage = "Invalid Student Id";
+    }
+  }
+
+  if (req.body.revenueCategoryId === 1) {
+    if (
+      util.isNullOrUndefined(req.body.totalFees) ||
+      util.isNullOrUndefined(req.body.paidFees) ||
+      util.isNullOrUndefined(req.body.balanceFees)
+    ) {
+      status = 400;
+      messsage = "Invalid Fee Details";
+    }
+  }
+
+  if (util.isInvalidAmount(req.body.amount)) {
+    status = 400;
+    messsage = "Amount cannot be zero";
+  }
+
+  if (status !== 0 || messsage !== "") {
+    res.status(status).send(messsage);
+  } else {
+    next();
+  }
 });
 
 router.post("/payment", (req, res) => {
