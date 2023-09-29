6d0d53b63fab6d8e3bf08c2cd71289cf85ba68de
Vaibhav <dev.vaibhav99@gmail.com>
2023-09-30 01:29:24 +0530

    Implemented newly created Error Handling classes

--- backend/middlewares/revenue.validation.middleware.js
+++ backend/middlewares/revenue.validation.middleware.js
@@ -1,19 +1,23 @@
 const util = require("../utils/revenue.utils");
-const {
-  INVALID_ID,
-  INVALID_USER_ID,
-  INVALID_REVENUE_CATEGORY_ID,
-  INVALID_STUDENT_ID,
-  AMOUNT_CANNOT_BE_ZERO,
-} = require("../utils/revenue.contants");
+const InvalidIdError = require("../errors/InvalidIdError");
+const InvalidFeeDetailsError = require("../errors/InvalidFeeDetailsError");
+const InvalidAmountError = require("../errors/InvalidAmountError");
 
 const deleteRevenueCategoryValidation = (req, res, next) => {
-  if (util.isInvalidId(req.params.id)) res.status(400).send(INVALID_ID);
+  if (util.isInvalidId(req.params.id))
+    throw new InvalidIdError(
+      "THE ID GIVEN TO DELETE REVENUE CATEGORY IS INVALID",
+      res
+    );
   else next();
 };
 
 const udpateRevenueCategoryByIdValidation = (req, res, next) => {
-  if (util.isInvalidId(req.params.id)) res.status(400).send(INVALID_ID);
+  if (util.isInvalidId(req.params.id))
+    throw new InvalidIdError(
+      "THE ID GIVEN TO UPDATE REVENUE CATEGORY IS INVALID",
+      res
+    );
   else next();
 };
 
@@ -22,11 +26,11 @@ const paymentValidation = (req, res, next) => {
     util.isNullOrUndefined(req.body.userId) ||
     util.isInvalidId(req.body.userId)
   ) {
-    res.status(400).send(INVALID_USER_ID);
+    throw new InvalidIdError("THE ENTERED USER ID IS INVALID", res);
   }
 
   if (util.isInvalidId(req.body.revenueCategoryId)) {
-    res.status(400).send(INVALID_REVENUE_CATEGORY_ID);
+    throw new InvalidIdError("THE ENTERED REVENUE CATEGORY ID IS INVALID", res);
   }
 
   if (
@@ -39,7 +43,7 @@ const paymentValidation = (req, res, next) => {
       util.isNullOrUndefined(req.body.studentId) ||
       util.isInvalidId(req.body.studentId)
     ) {
-      res.status(400).send(INVALID_STUDENT_ID);
+      throw new InvalidIdError("THE ENTERED STUDENT IS NOT VALID", res);
     }
   }
 
@@ -49,24 +53,34 @@ const paymentValidation = (req, res, next) => {
       util.isNullOrUndefined(req.body.paidFees) ||
       util.isNullOrUndefined(req.body.balanceFees)
     ) {
-      res.status(400).send(INVALID_FEE_DETAILS);
+      // res.status(400).send(INVALID_FEE_DETAILS);
+      throw new InvalidFeeDetailsError("FEE DETAILS MUST BE APPROPRIATE", res);
     }
   }
 
   if (util.isInvalidAmount(req.body.amount)) {
-    res.status(400).send(AMOUNT_CANNOT_BE_ZERO);
+    // res.status(400).send(AMOUNT_CANNOT_BE_ZERO);
+    throw new InvalidAmountError("CAN'T MAKE PAYMENT WITH 0 AMOUNT", res);
   }
 
   next();
 };
 
 const getIncomeDetilsByIdValidation = (req, res, next) => {
-  if (util.isInvalidId(req.params.id)) res.status(400).send(INVALID_ID);
+  if (util.isInvalidId(req.params.id))
+    throw new InvalidIdError(
+      "THE ID GIVEN TO FETCH INCOME DETAILS IS INVALID",
+      res
+    );
   else next();
 };
 
 const getExpenseDetilsByIdValidation = (req, res, next) => {
-  if (util.isInvalidId(req.params.id)) res.status(400).send(INVALID_ID);
+  if (util.isInvalidId(req.params.id))
+    throw new InvalidIdError(
+      "THE ID GIVEN TO FETCH EXPENSE DETAILS IS INVALID",
+      res
+    );
   else next();
 };
 
